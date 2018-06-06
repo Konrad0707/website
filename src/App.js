a17cced84d78a6c4692784ec7d55c043312ef89e
konrad0707 <4321konrad@gmail.com>
2018-06-06 16:53:50 +0800

    Update App.js and run npm build

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import React, { Component } from 'react';
-import Welcome from './components/Welcome'
-import Projects from './components/Projects'
-import Teaching from './components/Teaching'
-import Blog from './components/Blog'
-import About from './components/About'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import arrow from './images/arrow.gif'
+import about from './components/about.js'
+import contact from './components/contact.js'
+import projects from './components/projects.js'
+import resume from './components/projects.js'
+import menu from './components/menu.js'
+import bg from './images/topbanner.jpg'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <NavBar />
-          <Welcome />
+          <menu/>
           <div className="down-arrow">
-          <img src={arrow} alt="Sroll"  width="50px"/>
+          <img src={bg} alt="Sroll"  width="50px"/>
           </div>
-        <h2 className="heading" id="projects">PROJECTS</h2>
-          <Projects />
         <h2 className="heading" id="about">ABOUT</h2>
-          <About />
-        <h2 className="heading" id="blog">BLOG</h2>
-          <Blog />
-        <h2 className="heading" id="teaching">TEACHING</h2>
-          <Teaching />
-        <h2 className="heading" id="footer">LIKE WHAT YOU SEE?</h2>
-          <Footer />
+          <about />
+        <h2 className="heading" id="contact">CONTACT</h2>
+          <contact/>
+        <h2 className="heading" id="projects">PROJECTS</h2>
+          <projects/>
+        <h2 className="heading" id="resume">RESUME</h2>
+          <resume />
       </div>
     );
   }
---
+++ src/components/menu.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class menu extends Component {
+  render() {
+    return (
+      <div>
+        /*<ul className="menu">
+
+          <li className="nav-item nav-name">MGT</li>
+          <li className="nav-item nav-last"><a href="#footer" className="nav-link">Contact</a></li>
+          <li className="nav-item"><a href="#teaching" className="nav-link">Teaching</a></li>
+          <li className="nav-item"><a href="#blog" className="nav-link">Blog</a></li>
+          <li className="nav-item"><a href="#about" className="nav-link">About</a></li>
+          <li className="nav-item"><a href="#projects" className="nav-link">Projects</a></li>
+        </ul>*/
+      </div>
+    );
+  }
+}
+
+export default menu;
