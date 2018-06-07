ce65615fd1128e9ae133c8b7fc8c89ca5074314b
konrad0707 <4321konrad@gmail.com>
2018-06-07 14:15:16 +0800

    update navbar

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,15 +4,13 @@ class menu extends Component {
   render() {
     return (
       <div>
-        /*<ul className="menu">
-
+        <ul className="menu">
           <li className="nav-item nav-name">MGT</li>
-          <li className="nav-item nav-last"><a href="#footer" className="nav-link">Contact</a></li>
-          <li className="nav-item"><a href="#teaching" className="nav-link">Teaching</a></li>
-          <li className="nav-item"><a href="#blog" className="nav-link">Blog</a></li>
+          <li className="nav-item nav-last"><a href="#contact" className="nav-link">Contact</a></li>
+          <li className="nav-item"><a href="#resume" className="nav-link">Resume</a></li>
           <li className="nav-item"><a href="#about" className="nav-link">About</a></li>
           <li className="nav-item"><a href="#projects" className="nav-link">Projects</a></li>
-        </ul>*/
+        </ul>
       </div>
     );
   }
