05edbfec3334c92aa4519aedcbf6e10d8cb90394
konrad0707 <4321konrad@gmail.com>
2018-06-19 15:29:22 +0800

    Fixed single web app routing problems

--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,11 @@ import './major.scss';
 export default class about extends Component{
   render(){
     return(
-      <div>hi</div>
+      <div>
+        <h1>Kai-Wei 'Konrad' Wang </h1>
+        <h2>Software Developer, World Traveller, Boba Addict</h2>
+        <h1>Heres a little bit about me, I will be telling you guys everything about my life</h1>
+      </div>
     )
   }
 
--- src/index.js
+++ src/index.js
@@ -1,44 +1,50 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom'
-import './index.css';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
+import { HashRouter as Router} from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link';
 
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import About from './components/about.js'
 import Contact from './components/contact.js'
 import Projects from './components/projects.js'
-import Resume from './components/resume.js'
-import Menu from './components/menu.js'
+import Portfolio from './components/resume.js'
 import bg from './images/topbanner.jpg'
 
 class Page extends Component {
   render () {
     return (
       <App>
-      <div className="App">
-
-        <header>
-          <Menu/>
-        </header>
-
-
-          <img src={bg} alt=""  width="100%" height="auto"/>
-
-          </div>
-          <h1>Kai-Wei 'Konrad' Wang </h1>
-        <h2>Software Developer, World Traveller, Boba Addict</h2>
-        <h1>Heres a little bit about me, I will be telling you guys everything about my life</h1>
         <Router>
-          <Switch>
-            <Route exact path='/' component={'index.js'} />
-            <Route path='/resume' component={Resume} />
-            <Route path='/contact' component={Contact} />
-            <Route path='/projects'component={Projects} />
-          </Switch>
-      </Router>
+          <div>
+            <header>
+              <ul className = "menu">
+                <li><Link to="Page#About">About</Link></li>
+                <li><Link to="Page#Portfolio">Portfolio</Link></li>
+                <li><Link to="Page#Projects">Projects</Link></li>
+                <li><Link to="Page#Contact">Contact</Link></li>
 
-      </App>
+              </ul>
+            </header>
+            <div className = "content">
+              <div id="About">
+                <About />
+              </div>
+              <img src={bg} alt=""  width="5%" height="auto"/>
+              <div id="Portfolio">
+                <Portfolio />
+              </div>
+              <div id = "Projects">
+                <Projects />
+              </div>
+              <div id="Contact">
+                <Contact />
+              </div>
+
+            </div>
+          </div>
+        </Router>
+    </App>
     )
   }
 }
