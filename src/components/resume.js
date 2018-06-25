36aa05975178d7e397d759d9e10b5f0326fa747f
konrad0707 <4321konrad@gmail.com>
2018-06-25 17:49:51 +0800

    Fix download resume button

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import './major.css';
+import file from './files/Kai-WeiWangResume.pdf';
 
 export default class resume extends Component{
   render(){
     return(
-      <form method="get" action="../files/Kai-WeiWangResume.pdf">
-        <button type="submit">Download!</button>
-      </form>
+      <a href={file} download>Click to Download resume</a>
     )
   }
 
