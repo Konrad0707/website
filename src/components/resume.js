9941cc9f53fa1c2e17a16b27446e9ed25c3615ff
konrad0707 <4321konrad@gmail.com>
2018-06-27 17:54:52 +0800

    Create basic flex structure for portfolio section

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
 import './major.css';
 import file from './files/Kai-WeiWangResume.pdf';
+import BluedLogo from '../images/bluedLogo.png'
 
 export default class resume extends Component{
   render(){
     return(
-      <a href={file} download>Click to Download resume</a>
+      <div className = 'resume'>
+        <h1> <strong>Here's what I've done so far</strong></h1>
+        <a href={file} download>Click to Download resume</a>
+        <div className = "container_exp">
+          <ul>
+            <li>
+              <div className = "content">
+                <div className = "imagebox">
+                  <img src = {BluedLogo} alt = ""/>
+                </div>
+                <h2>Blued</h2>
+                <h4><strong><span className = "job_title">Software Engineer Intern</span><br></br>
+                Beijing, China</strong></h4>
+                <p>Front End Engineer</p>
+              </div>
+            </li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+
+        </div>
+      </div>
     )
   }
 
