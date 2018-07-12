108e745a57dc235d3c8a7f79a5b7f4459594f483
konrad0707 <4321konrad@gmail.com>
2018-07-12 14:58:52 +0800

    update contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,7 +10,7 @@ export default class contact extends Component{
         <footer>
           <h1>Whoop!! If im interesting enough for you to scroll through everything,<br></br>Then we should definitely get in touch!</h1>
           <h2><span><a href="http://www.linkedin.com/in/kwang0707">Linkedin </a></span>
-           or Email works.</h2>
+           or <span><a href="http://www.linkedin.com/in/kwang0707">Email</a></span> works.</h2>
           <a href="http://www.linkedin.com/in/kwang0707"><img src={linkedin} alt=""/></a>
           <img src={email} alt=""/>
         </footer>
