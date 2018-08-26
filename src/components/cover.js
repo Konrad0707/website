40a220122140d14a30ba48ecc423299887a4798f
Konrad Wang <4321konrad@gmail.com>
2018-08-26 14:56:41 -0400

    change scroll options

--- src/components/cover.js
+++ src/components/cover.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import './major.css';
 
+import Down from '../images/down.png'
+
 export default class projects extends Component{
   render(){
     return(
@@ -8,6 +10,9 @@ export default class projects extends Component{
         <div className = "Title">
           <h1><mark>Kai-Wei 'Konrad' Wang </mark></h1>
           <h2><mark>Software Developer<br/>World Traveller<br/> Boba Addict</mark></h2>
+        </div>
+        <div className = "DownMarker">
+          <img src={Down} alt="" width="50px" height="auto"/>
         </div>
       </div>
     )
